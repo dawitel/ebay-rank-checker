4e39a3309e73a8ae2650759ab6a7e814213a4171
dawitel <dawiteliaskassaye@gmail.com>
2024-09-01 03:22:17 +0300

    feat: add the landing page, the uploading component and toast messages I created the landing page using next js and shadcn ui for components I also created the file upload component to upload the CSV files Lastly, I created the toast messages for defferent cases

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Eaby Rank Checker",
+  description: "Scrape and rank products from ebay",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+      <Toaster />
+    </html>
+  );
+}
---
+++ src/components/fileupload.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { ToastAction } from "@radix-ui/react-toast";
+
+interface CsvUploadProps {
+  onFileUpload: (file: File) => void;
+}
+
+const CsvUpload: React.FC<CsvUploadProps> = ({ onFileUpload }) => {
+  const { toast } = useToast();
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.type === "text/csv") {
+      setSelectedFile(file);
+    } else {
+      toast({
+        variant: "destructive",
+        title: "The file you're trying to upload is not supported!",
+        description: "Try uploading a CSV file",
+        action: (
+          <ToastAction altText="cancel">Cancel</ToastAction>
+        ),
+      });
+    }
+  };
+
+  const handleUploadClick = () => {
+    if (selectedFile) {
+      onFileUpload(selectedFile);
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Please select a file to upload!",
+        description: "Try uploading a CSV file",
+      });
+    }
+  };
+
+  return (
+    <div className="flex space-x-4">
+      <Input
+        type="file"
+        accept=".csv"
+        onChange={handleFileChange}
+        className="bg-slate-200 h-9.5 text-slate-900"
+      />
+      <Button
+        onClick={handleUploadClick}
+        className="bg-blue-500 hover:bg-blue-500 hover:text-gray-200 text-white px-4 py-2 rounded"
+      >
+        Upload CSV File
+      </Button>
+    </div>
+  );
+};
+
+export default CsvUpload;
